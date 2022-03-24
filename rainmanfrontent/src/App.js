70c4784bbb075bb46bc8d0009333a09477b2a71d
nathankaufman <nlk7665@rit.edu>
2022-03-24 15:55:50 -0400

    Working react router

--- rainmanfrontent/src/App.js
+++ rainmanfrontent/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import react from 'react';
+import Login from './components/login';
+import Landing from './components/landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />}/>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
