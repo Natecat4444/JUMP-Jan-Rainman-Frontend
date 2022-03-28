ed3cc00f7709f68464c42cc65afdbfbad4f0e866
herrickc9614 <46943963+herrickc9614@users.noreply.github.com>
2022-03-28 16:56:51 -0500

    Update App.js

--- rainmanfrontent/src/App.js
+++ rainmanfrontent/src/App.js
@@ -1,10 +1,4 @@
 import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import react from 'react';
-import Login from './components/login';
-import Landing from './components/landing';
-import SignUp from './components/signup';
-import CreateBet from './components/createBet';
 import Header from './components/nav';
 
 function App() {
