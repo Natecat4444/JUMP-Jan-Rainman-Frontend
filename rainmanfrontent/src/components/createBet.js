9aaca267d3360bfc6132b770c78d7d00807f2976
nathankaufman <nlk7665@rit.edu>
2022-03-29 12:30:21 -0400

    City now being stored properly in bets

--- rainmanfrontent/src/components/createBet.js
+++ rainmanfrontent/src/components/createBet.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import RainUserAPI from '../APIs/RainUserAPI'
 import { CityList } from './inner/CitySelect';
 
@@ -7,6 +7,11 @@ function CreateBet(props){
     const [credits, setCredits] = useState(1);
     const [date, setDate] = useState("")
     const [temp, setTemp] = useState(0)
+    const[city, setCity] = useState('')
+
+    const changeCity = (value) =>{
+        setCity(value)
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -14,22 +19,25 @@ function CreateBet(props){
         const bet = {
             "bet_id": null,
             "user_id": null,
-            "city_id": viewhandler,
+            "city": city,
             "wager": credits,
             "temperature": temp,
             "forecast_date": date,
             "status": "PENDING"
         }
-
         console.log(bet);
 
         RainUserAPI.createBet(bet)
+
+        
+
+        
     }
     return(
         <div>
             <form onSubmit={handleSubmit}>
                 <label>Credits: <input type="number" min="1" step='1' value={credits} onChange={(event)=>{setCredits(event.target.value)}}/></label> <br />
-                <label>City: <CityList name="citySelect" viewhandler={setviewhandler} /></label> <br />
+                <label>City: <CityList name="citySelect" viewhandler={changeCity} /></label> <br />
                 <label>Date: <input type="date" value={date} onChange={(event)=>{setDate(event.target.value)}}/></label> <br />
                 <label>Temperature: <input type="number" step='1' value={temp} onChange={(event)=>{setTemp(event.target.value)}}/></label> <br />
                 <input type="submit" value="Create Bet" />
