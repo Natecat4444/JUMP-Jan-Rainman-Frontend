3dc5fb3b85a3b17e42499895cfe4e3123c6ae2fe
nathankaufman <nlk7665@rit.edu>
2022-03-31 10:59:26 -0400

    added landing stuff

--- rainmanfrontent/src/components/landing.js
+++ rainmanfrontent/src/components/landing.js
@@ -6,7 +6,7 @@ function Landing(){
         <div className = "routeTitle">
           <h1>Landing</h1>
         </div>
-            <p>this is temp</p>
+            <p>Welcome to Weather Better! <br /> If your account is not visible up to please <a href="/login">Login</a> or <a href="/signup">Sign Up</a> to use</p>
         </div>
     )
 }
