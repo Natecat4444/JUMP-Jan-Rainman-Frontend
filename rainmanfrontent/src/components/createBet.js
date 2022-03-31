26e15bcaa2caeee6860efee75dec221ee4de2f56
herrickc9614 <46943963+herrickc9614@users.noreply.github.com>
2022-03-31 11:07:09 -0500

    finish css

--- rainmanfrontent/src/components/createBet.js
+++ rainmanfrontent/src/components/createBet.js
@@ -42,7 +42,7 @@ function CreateBet(props){
             <div className = 'form'>
                 <form onSubmit={handleSubmit}>
                     <div className="formElement"><label>Credits</label> <input type="number" min="1" step='1' value={credits} onChange={(event)=>{setCredits(event.target.value)}}/></div> <br />
-                    <div className="formElement"><label>City</label> <CityList name="citySelect" viewhandler={changeCity} /> <br /></div>
+                    <div className="formElementCity"><label>City</label> <CityList name="citySelect" viewhandler={changeCity} /> <br /></div>
                     <div className="formElement"><label>Date</label> <input type="date" value={date} onChange={(event)=>{setDate(event.target.value)}}/></div> <br />
                     <div className="formElement"><label>Temperature</label> <input type="number" step='1' value={temp} onChange={(event)=>{setTemp(event.target.value)}}/></div> <br />
                     <input type="submit" value="Create Bet" />
